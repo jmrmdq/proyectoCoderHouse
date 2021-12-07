52d2396105fc029fbe6c36d2b8161361e575e7c9
Martín Rodríguez <jmrmdq@gmail.com>
2021-12-07 20:34:09 -0300

    Agregue carpeta de funciones js y valide los campos

--- js/codigojs.js
+++ js/codigojs.js
@@ -1,12 +1,12 @@
 // armar la clase para los usuarios nuevos
 class Usuarios{
     constructor(nombre, apellido, osocial, edad, celu, documento){
-        this.nombre=nombre;
-        this.apellido=apellido;
-        this.osocial=osocial;
-        this.edad=edad;
-        this.celu=celu;
-        this.documento=documento;
+        this.nombre=nombre.toUpperCase();
+        this.apellido=apellido.toUpperCase();
+        this.osocial=osocial.toUpperCase();
+        this.edad= parseInt(edad);
+        this.celu= parseInt(celu);
+        this.documento= parseInt(documento);
     }
 }
 
@@ -23,16 +23,25 @@ formulario.addEventListener("submit", procesarFormulario);
 function procesarFormulario(e) {
     e.preventDefault();
     let obtenerFormulario = e.target;
-    // genero constantes con los datos obtenidos
-    const nombre = obtenerFormulario.children[1].value;
-    const apellido = obtenerFormulario.children[3].value;
-    const osocial = obtenerFormulario.children[5].value;
-    const edad = obtenerFormulario.children[7].value;
-    const celu = obtenerFormulario.children[9].value;
-    const documento = obtenerFormulario.children[11].value;
+
+// Valido si los campos contienen texto y numero segu corresponda c/u y genero la constante para mi array
+    datoValido(obtenerFormulario.children[1].value);
+    const nombre = datoOk;
+    datoValido(obtenerFormulario.children[3].value);
+    const apellido = datoOk;
+    datoValido(obtenerFormulario.children[5].value);
+    const osocial = datoOk;
+    numeroValido(obtenerFormulario.children[7].value);
+    const edad = numeroOk;
+    numeroValido(obtenerFormulario.children[9].value);
+    const celu = numeroOk;
+    numeroValido(obtenerFormulario.children[11].value);
+    const documento = numeroOk;
+    
     // comienzo a volcar esta info a mi array de usuarios
     UsuariosRegistrados.push(new Usuarios (nombre, apellido, osocial, edad, celu, documento));
     console.log(UsuariosRegistrados);
+
     // Guardo el usuario en el local Storage, previo, lo paso a JSON
     const URegistradoEnJSON = (clave, valor)=> {localStorage.setItem(clave, valor)};
 
@@ -40,19 +49,10 @@ function procesarFormulario(e) {
         URegistradoEnJSON(usuarioReg.documento, JSON.stringify(UsuariosRegistrados));
     }
 }
-// tion validarFormulario (e){
+// 
+//EnJSON = (clave, valor)=> {localStorage.setItem(clave, valor)};
 
-// //Cancelamos el comportamiento del evento
-// e.preventDefault ();
-
-// //Obtenemos el elemento desde el cual se disparó el evento
-// let formulario = e.target
-
-// //Obtengo el valor del primero hijo <input type="text">
-// console.log(formulario.children[ 0].value);
-
-// //Obtengo el valor del segundo hijo <input type="number">
-// console.log(formulario.children[ 1].value);
-// }ut type="number">
-// console.log(formulario.children[ 1].value);
+//     for (const usuarioReg of UsuariosRegistrados){
+//         URegistradoEnJSON(usuarioReg.documento, JSON.stringify(UsuariosRegistrados));
+//     }
 // }
---
+++ js/funciones.js
@@ -0,0 +1,33 @@
+// funcion para validar datos en formulario
+let datoOk = null;
+let numeroOk = 0;
+
+function datoValido (nombre) {
+    if (nombre == parseInt(nombre)) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Algo salió mal, ingresa un texto!',
+            footer: '<a href="">Necesitas ayuda, ingresa aqui!!</a>'
+          })
+    } else {
+        datoOk = nombre;
+    }
+   
+}
+
+function numeroValido(numero) {
+    if (numero == parseInt(numero)) {
+        numeroOk = numero;        
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Algo salió mal, ingresa numero sin puntos o comillas!',
+            footer: '<a href="">Necesitas ayuda, ingresa aqui!!</a>'
+        })
+    }  
+}
+//     }
+   
+// }
